3fe56343f4165aa547b52a4f114ea740182aa52c
Sarvagya Pant <sarvagya@Sarvagyas-MacBook-Pro.local>
2017-05-16 17:23:18 -0500

    completed coin making. started fractional knapsack

--- Week 2/greedy_algorithms/change/change.cpp
+++ Week 2/greedy_algorithms/change/change.cpp
@@ -1,12 +1,28 @@
 #include <iostream>
 
+
+// coin denominations are 1,5,10
 int get_change(int m) {
   //write your code here
-  return n;
+    if(m < 5)
+        // if coin is less than 5, we need same # of 1-coins.
+        return m;
+    else if(m == 5) 
+        // if its 5, handle a single 5-coin
+        return 1;
+    else if(m > 5 && m < 10) 
+        // if its greater than 5 but less than 10, handle a single 5-coin and make change for (total - 5)
+        return 1 + get_change(m - 5);
+    else if(m  == 10) 
+        // if its 10 handle a single 10-coin
+        return 1;
+    else 
+        // if its greater than 10, handle a single 10-coin and make change for (total - 10)
+        return 1 + get_change(m-10);
 }
 
 int main() {
-  int m;
-  std::cin >> m;
-  std::cout << get_change(m) << '\n';
+    int m;
+    std::cin >> m;
+    std::cout << get_change(m) << '\n';
 }
--- Week 2/greedy_algorithms/fractional_knapsack/fractional_knapsack.cpp
+++ Week 2/greedy_algorithms/fractional_knapsack/fractional_knapsack.cpp
@@ -2,8 +2,9 @@
 #include <vector>
 
 using std::vector;
+typedef vector<int> vi;
 
-double get_optimal_value(int capacity, vector<int> weights, vector<int> values) {
+double get_optimal_value(int capacity, vi weights, vi values) {
   double value = 0.0;
 
   // write your code here
@@ -15,8 +16,8 @@ int main() {
   int n;
   int capacity;
   std::cin >> n >> capacity;
-  vector<int> values(n);
-  vector<int> weights(n);
+  vi values(n);
+  vi weights(n);
   for (int i = 0; i < n; i++) {
     std::cin >> values[i] >> weights[i];
   }
