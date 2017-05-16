98937b4173c814d8662cd9bb5dd4f2e4f4f96b20
Sarvagya Pant <sarvagya@Sarvagyas-MacBook-Pro.local>
2017-05-16 18:17:26 -0500

    fractional knapsack done

--- Week 2/greedy_algorithms/fractional_knapsack/fractional_knapsack.cpp
+++ Week 2/greedy_algorithms/fractional_knapsack/fractional_knapsack.cpp
@@ -1,30 +1,56 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using std::vector;
 typedef vector<int> vi;
 
-double get_optimal_value(int capacity, vi weights, vi values) {
-  double value = 0.0;
+typedef struct item {
+    int weight, value;
+} Item;
 
-  // write your code here
+typedef std::vector<Item> v_item;
 
-  return value;
+bool cmp(Item a, Item b)
+{
+    double r1 = (double)a.value / a.weight;
+    double r2 = (double)b.value / b.weight;
+    return r1 > r2;
+}
+double get_optimal_value(int capacity, v_item items, int n)
+{
+    double value = 0.0;
+    // write your code here
+    sort(items.begin(), items.end(), cmp); // each item is now sorted on the basis of value per weight.
+
+    for (int i = 0; i < items.size(); ++i) {
+        if (capacity == 0)
+            return value;
+        else {
+            int actual = std::min(items[i].weight, capacity);
+            value += actual * ((double)items[i].value / items[i].weight);
+            items[i].weight -= actual;
+            capacity -= actual;
+        }
+    }
+    return value;
 }
 
-int main() {
-  int n;
-  int capacity;
-  std::cin >> n >> capacity;
-  vi values(n);
-  vi weights(n);
-  for (int i = 0; i < n; i++) {
-    std::cin >> values[i] >> weights[i];
-  }
+int main()
+{
+    int n;
+    int capacity;
+    std::cin >> n >> capacity;
+    v_item items;
+    for (int i = 0; i < n; i++) {
+        Item it;
+        std::cin >> it.value >> it.weight;
+        items.push_back(it);
+    }
 
-  double optimal_value = get_optimal_value(capacity, weights, values);
+    double optimal_value = get_optimal_value(capacity, items, n);
 
-  std::cout.precision(10);
-  std::cout << optimal_value << std::endl;
-  return 0;
+    std::cout.precision(10);
+    std::cout << optimal_value << std::endl;
+    return 0;
 }
