6d4c96c046f15de27b111af629ed268819c94178
Sarvagya Pant <sarvagya@Sarvagyas-MacBook-Pro.local>
2017-05-16 21:26:11 -0500

    covering segment done

--- Week 2/greedy_algorithms/covering_segments/covering_segments.cpp
+++ Week 2/greedy_algorithms/covering_segments/covering_segments.cpp
@@ -6,27 +6,42 @@
 using std::vector;
 
 struct Segment {
-  int start, end;
+  int starting, ending;
 };
 
-vector<int> optimal_points(vector<Segment> &segments) {
-  vector<int> points;
+typedef vector<Segment> v_seg;
+typedef vector<int> vi;
+
+bool cmp(Segment a, Segment b){
+  int r1 = a.ending;
+  int r2 = b.ending;
+  return r1 < r2;
+}
+
+vi optimal_points(v_seg &segments) {
+  vi points;
   //write your code here
-  for (size_t i = 0; i < segments.size(); ++i) {
-    points.push_back(segments[i].start);
-    points.push_back(segments[i].end);
+  sort(segments.begin(), segments.end(), cmp); // sort the Segments based on the ending positions.
+
+  int current = segments[0].ending;
+  points.push_back(current);
+  for(int i=0; i< segments.size(); i++) {
+    if (current < segments[i].starting || current > segments[i].ending) {
+      current = segments[i].ending;
+      points.push_back(current);
+    }
   }
-  return points;
+return points;
 }
 
 int main() {
   int n;
   std::cin >> n;
-  vector<Segment> segments(n);
+  v_seg segments(n);
   for (size_t i = 0; i < segments.size(); ++i) {
-    std::cin >> segments[i].start >> segments[i].end;
+    std::cin >> segments[i].starting >> segments[i].ending;
   }
-  vector<int> points = optimal_points(segments);
+  vi points = optimal_points(segments);
   std::cout << points.size() << "\n";
   for (size_t i = 0; i < points.size(); ++i) {
     std::cout << points[i] << " ";
